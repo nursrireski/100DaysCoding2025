c915cc2a2b22c523147c96916bca047bfc50677e
nursrireski <kikipolman2324@gmail.com>
2025-09-09 16:34:25 +0700

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,24 @@
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("Saya akan bertahan selama 100 Hari Ngoding ,dan akan menjadi anggota resmi ISC");
+        System.out.println();
+        
+        System.out.print("Selamat telah");
+        System.out.print(" menjadi anggota ISC ");
+        System.out.println("");
+        
+        
+        String nama = "NUR SRI RESKI";
+        String nim = "D0224305";
+        char kelas = 'A';
+        byte umur = 20;
+        int tinggiBadan = 154;
+        System.out.printf("|%-6s : %-10s | %n","Nama","(" + nama + ")");
+        System.out.printf("|%-6s : %-15s | %n","Nim","(" + nim + ")");
+        System.out.printf("|%-6s : %-15s | %n","Kelas","(" + kelas + ")");
+        System.out.printf("|%-6s : %-15s | %n","Umur","(" + umur + ")");
+        System.out.printf("|%-6s : %-15s | %n","TB","(" + tinggiBadan + ")");
+        System.out.println();
+    }
+    
+}
