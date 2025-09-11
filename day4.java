8828d8f2fe3e35c8d1f5e541524778d8b8c8eb10
nursrireski <kikipolman2324@gmail.com>
2025-09-11 15:51:29 +0700

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,36 @@
+public class day4 {
+    public static void main(String[] args) {
+        // 1. \n(New Line)
+        System.out.println("1.Menggunakan New Line");
+        System.out.print("Anak Pertama\nAnak Kedua ");
+        System.out.println("\n");
+        // 2. \t(Horizontal Tab)
+        System.out.println("2.Menggunakan Horizontal Tab");
+        System.out.println("Anak Pertama\t: Ali Imran");
+        System.out.println("Anak Kedua\t: Indah Khairun Nisa");
+        System.out.println("\t");
+        // 3.\\ Backslash
+        System.out.println("3.Menggunakan Backslash");
+        System.out.println("Anak Ketiga\\Fatimah Ulfa");
+        System.out.println("");
+        // 4. \"Double Quote
+        System.out.println("4.Menggunakan Double Quote");
+        System.out.println("\"Anak Keempat: NUR SRI RESKI\"");
+        // 5.\' Single Quote
+        System.out.println("5.Mengunakan Single Quote");
+        System.out.println("\'D0224305\'"); 
+        // 6.\b Backspace
+        System.out.println("6.Menggunakan Backspace");
+        System.out.println("Nama saya NUR\b\b\bSRI RESKI");
+         // 7.\r Carriage Return
+        System.out.println("7.Menggunakan Carriage Return");
+        System.out.println("Nur Sri Reski... \rKiki");
+        // 8. Gabungan
+        System.out.println("8.Menggunakan Gabungan");
+        System.out.println("Dia berkata, \"Halo Dunia!\"");
+        System.out.println("Lokasi file ada di C:\\Users\\Public");
+
+
+    }
+    
+}
