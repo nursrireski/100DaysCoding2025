7f0dd34229d7de7c6576d6cc33f6bef95748ca9d
nursrireski <kikipolman2324@gmail.com>
2025-09-12 17:39:24 +0700

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,20 @@
+public class day5 {
+    public static void main(String[] args) {
+        //byte kapasitas nilai (-128 hingga 127) ukuran(8-bit)
+        byte jumlahJari = 10;
+        System.out.println("byte: "+jumlahJari);
+               
+        //short kapasitas nilain (-32,768 hingga 32,767) ukuran(16-bit)
+        short hargaJilbab = 20000;
+        System.out.println("short: "+hargaJilbab);
+        
+        //int(Integer)kapasitas nilain (-2,147,483,648 hingga 2,147,483,647) ukuran(32-bit)
+        int luasLapangan = 20;
+        System.out.println("integer: "+luasLapangan);
+        
+        
+        //long kapasitas nilai(-9,223,372,036,854,775,808 hingga 9,223,372,036,854,775,807) ukuran(64-bit)
+        long hargaRumah = 350000000000l;
+        System.out.println("long: "+hargaRumah);
+    }
+}
