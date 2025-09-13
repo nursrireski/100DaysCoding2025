f8dc9bcf5616a28a88c009e021121f86a008e15e
nursrireski <kikipolman2324@gmail.com>
2025-09-13 17:30:41 +0700

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,11 @@
+public class day6 {
+    public static void main(String[] args) {
+        //float mampu menyimpan sekitar 6 hingga 7 digit desimal secara akurat
+        float x = 36.5f;
+        System.out.println("float: "+x);
+        //double mampu menyimpan sekitar 15 digit desimal secara akurat
+        double X = 6795.4678;
+        System.out.println("double: "+X);
+    }
+    
+}
