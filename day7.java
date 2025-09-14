ac1b7c9b556639ad77ce1514b581b7285633d268
nursrireski <kikipolman2324@gmail.com>
2025-09-14 18:23:13 +0700

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,32 @@
+public class day7 {
+    public static void main(String[] args) {
+        // char adalah tipe data yangg digunakan untuk menyipan satu karakter tunggal
+         char ha   = 'ه';
+        char ya   = 'ي';
+        char alif = 'ا';
+
+        char nun  = 'ن';
+        char ta   = 'ت';
+        char ain  = 'ع';
+        char lam  = 'ل';
+        char lam2 = 'ل';
+        char mim  = 'م';
+       
+        System.out.print(ha);
+        System.out.print(ya);
+        System.out.print(alif);
+        System.out.print(nun);
+        System.out.print(ta);
+        System.out.print(ain);
+        System.out.print(lam);
+        System.out.print(lam2);
+        System.out.print(mim);
+        System.out.println();
+        
+        //boolean adalah tipe data yang paling sederhana yang hanya memiliki salah satu dari dua nilai true(benar)atau false(salah)
+        boolean belajar = true;
+        System.out.println("Apakah Kia sedang belajar? "+ belajar);
+        
+        
+    }
+}
