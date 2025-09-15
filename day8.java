8be4a9aecd5554343c28b0d963012819d686f3e7
nursrireski <kikipolman2324@gmail.com>
2025-09-15 18:49:42 +0700

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,24 @@
+public class day8 {
+    public static void main(String[] args) {
+        String alasanMasukISC = "Alasan saya masuk ISC yaitu saya ingin belajar lebih dalam "
+                + "bahasa pemrograman\nterutama java dan semua yang berbaur dengan IT"; 
+        
+        String alasanMemilihInformatika = "Alasan saya memilih prodi Informatika yaitu saya sangat suka hal-hal yang "
+                + "berhubungan dengan IT terutama Jaringan komputer.\nSaya merasa dengan masuk ke prodi Informatika "
+                + "saya dapat belajar lebih banyak tentang jaringan komputer"
+                + "dan suatu saat saya\ndapat menciptakan aplikasi hasil buatan saya di keamanan jaringan."; 
+        
+        String salahPilihJurusanAtauTidak = "Saya tidak salah pilih jurusan. Dari awal pendaftaran saya "
+                + "hanya memilih Prodi Informatika"; 
+        
+        String kendala = "Kendala saya yaitu pada saat semester 1 di mata kuliah DDP saya kaget tiba-tiba belajar kodingan "
+                + "walaupun saya\nalumni dari SMK tetapi saya tidak pernah belajar bahasa pemrograman Java "
+                + "dan kurang paham dalam membaca soal.\nPada semester 2 di mata kuliah PBO saya memiliki kebiasaan "
+                + "yang buruk yaitu belajar hanya di hari itu saat PBO,\njadi itulah mengapa saya mendapat nilai E."; 
+               
+        System.out.println("1. Alasan Masuk ISC:\n" + alasanMasukISC +"\n");        
+        System.out.println("2. Alasan Memilih Prodi Informatika:\n" + alasanMemilihInformatika + "\n");        
+        System.out.println("3. Apakah salah pilih jurusan atau Tidak:\n" + salahPilihJurusanAtauTidak + "\n");        
+        System.out.println("4. Kendala saya selama di Prodi Informatika:\n" + kendala);        
+    }
+}
