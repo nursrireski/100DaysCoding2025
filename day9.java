3a96e566768e070384f4ace8e5f6baab4f88933e
nursrireski <kikipolman2324@gmail.com>
2025-09-16 17:06:48 +0700

    Add files via upload

---
+++ day9.java
@@ -0,0 +1,31 @@
+public class day9 {
+    public static void main(String[] args) {
+        //Saat deklarai variabel
+        String nama;
+        String nim;
+        String programStudi;
+        String fakultas;
+        
+        
+        //inisialisasi
+        nama = "nur sri reski";
+        nim = "d022435";
+        programStudi = "informatika";
+        
+        System.out.printf("%-15s : %-10s  %n","Nama", nama );
+        System.out.printf("%-15s : %-10s  %n","Nim", nim );
+        System.out.printf("%-15s : %-10s  %n","Program Studi", programStudi);
+        System.out.println();
+        //Update
+        System.out.println("===Update Data===");
+        nama = "NUR SRI RESKI";
+        nim = "D0224305";
+        programStudi = "INFORMATIKA";
+        
+        System.out.printf("%-15s : %-10s  %n","Nama", nama );
+        System.out.printf("%-15s : %-10s  %n","Nim", nim );
+        System.out.printf("%-15s : %-10s  %n","Program Studi", programStudi);
+
+            }
+    
+}
