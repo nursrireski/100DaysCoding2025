6770b224e62f84e2164703fc5d174fcbc79e34eb
nursrireski <kikipolman2324@gmail.com>
2025-09-17 19:58:18 +0700

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,10 @@
+public class day10 {
+    public static void main(String[] args) {
+        int umur = 16;
+        
+        // umur minimal untuk membuat sim adalah 17 tahun
+        final int umurMinimalBuatSIM = 17;
+        System.out.println("Apakah boleh membuat SIM ?: "+(umur >= umurMinimalBuatSIM));
+    }
+    
+}
