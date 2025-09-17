db1bc2edd06d7e0033ae5c4a27af35766f709e17
nursrireski <kikipolman2324@gmail.com>
2025-09-17 20:31:04 +0700

    Update day10.java

--- day10.java
+++ day10.java
@@ -1,10 +1,13 @@
 public class day10 {
     public static void main(String[] args) {
-        int umur = 16;
-        
+      int umur = 16;
         // umur minimal untuk membuat sim adalah 17 tahun
         final int umurMinimalBuatSIM = 17;
-        System.out.println("Apakah boleh membuat SIM ?: "+(umur >= umurMinimalBuatSIM));
+        final boolean buatSIM = false;
+        System.out.println("Umur: "+ umur);
+        System.out.println("Apakah Boleh Membuat SIM : "+buatSIM); 
+    
     }
     
 }
+
