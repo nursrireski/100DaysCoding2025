bab36349b9f87ef01b510dd7ea95bb2faba74e5e
nursrireski <kikipolman2324@gmail.com>
2025-10-16 19:35:33 +0700

    Update day39.java

--- day39.java
+++ day39.java
@@ -4,35 +4,26 @@ public class day39 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("========================================");
-        System.out.println(" SELAMAT DATANG DI WARUNG MINUM SEGAR ");
-        System.out.println("========================================");
-        System.out.println("Silakan pilih minuman yang Anda inginkan:");
-        System.out.println("----------------------------------------");
-        System.out.println("1. Es Teh       - Rp 5.000");
-        System.out.println("2. Es Jeruk     - Rp 7.000");
-        System.out.println("3. Kopi Susu    - Rp 8.000");
-        System.out.println("----------------------------------------");
-        System.out.print("Masukkan nomor pilihan Anda (1-3): ");
+        // Cetakan 1
+        System.out.println(" WARUNG MINUM SEGAR \n1. Es Teh (Rp 5.000)\n2. Es Jeruk (Rp 7.000)\n3. Kopi Susu (Rp 8.000)\n");
 
+        System.out.print("Masukkan nomor pilihan (1-3): ");
         int pilihan = input.nextInt();
-        System.out.println("----------------------------------------");
 
-        if (pilihan == 1) {
-            System.out.println("Anda memesan Es Teh dengan harga Rp 5.000.");
-            System.out.println("Segarnya bikin hari makin adem!");
-        } else if (pilihan == 2) {
-            System.out.println("Anda memesan Es Jeruk dengan harga Rp 7.000.");
-            System.out.println("Rasanya manis dan menyegarkan!");
-        } else if (pilihan == 3) {
-            System.out.println("Anda memesan Kopi Susu dengan harga Rp 8.000.");
-            System.out.println("Nikmatnya bikin semangat kembali! ");
-        } else {
-            System.out.println("⚠ Pilihan tidak valid. Silakan jalankan ulang program dan pilih 1–3.");
-        }
+        String pesan;
 
-        System.out.println("----------------------------------------");
-        System.out.println("Terima kasih telah memesan di Warung Minum Segar!");
-        System.out.println("========================================");
+        if (pilihan == 1)
+            pesan = "Anda memilih  Es Teh - Rp 5.000.\nSegarnya bikin adem!";
+        else if (pilihan == 2)
+            pesan = "Anda memilih  Es Jeruk - Rp 7.000.\nRasanya manis dan segar!";
+        else if (pilihan == 3)
+            pesan = "Anda memilih  Kopi Susu - Rp 8.000.\nNikmatnya bikin semangat!";
+        else
+            pesan = "Pilihan tidak valid. Silakan jalankan ulang.";
+
+        // Cetakan 2
+        System.out.println("\n" + pesan + "\nTerima kasih telah memesan di Warung Minum Segar!");
+
+        
     }
 }
