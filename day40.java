4860a4157609d4d7bb9dd006aa1a92009e50b970
nursrireski <kikipolman2324@gmail.com>
2025-10-17 21:16:11 +0700

    Update day40.java

--- day40.java
+++ day40.java
@@ -3,44 +3,61 @@ import java.util.Scanner;
 public class day40 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        
         System.out.print("Masukkan angka pertama: ");
-        int angka1 = input.nextInt();
+        double angka1 = input.nextDouble();
 
         System.out.print("Masukkan operator (+, -, *, /): ");
         char operator = input.next().charAt(0);
 
         System.out.print("Masukkan angka kedua: ");
-        int angka2 = input.nextInt();
+        double angka2 = input.nextDouble();
 
-        int hasil;
-
+        double hasil = 0;
         
+
         if (operator == '+') {
             hasil = angka1 + angka2;
-            System.out.println("Hasil: " + hasil);
+             if (hasil % 1 == 0) {
+            System.out.println("Hasil: " + (int) hasil);  
+        } else {
+            System.out.println("Hasil: " + hasil);        
+        }
         } 
         else if (operator == '-') {
             hasil = angka1 - angka2;
-            System.out.println("Hasil: " + hasil);
+             if (hasil % 1 == 0) {
+            System.out.println("Hasil: " + (int) hasil);  
+        } else {
+            System.out.println("Hasil: " + hasil);        
+        }
         } 
         else if (operator == '*') {
             hasil = angka1 * angka2;
-            System.out.println("Hasil: " + hasil);
+             if (hasil % 1 == 0) {
+            System.out.println("Hasil: " + (int) hasil);  
+        } else {
+            System.out.println("Hasil: " + hasil);        
+        }
         } 
         else if (operator == '/') {
             if (angka2 != 0) {
                 hasil = angka1 / angka2;
-                System.out.println("Hasil: " + hasil);
-            } else {
+                } else {
                 System.out.println("Error: Pembagian dengan nol tidak diperbolehkan!");
+            }
+            if(angka1 % 1 == 0){
+                hasil = angka1 / angka2;
+            System.out.println("Hasil: " + (int) hasil);  
+            } else {
+            System.out.println("Hasil: " + hasil);        
+    
             }
         } 
         else {
             System.out.println("Operator tidak dikenali. Gunakan +, -, *, atau /.");
+          
         }
 
-
+       
     }
 }
