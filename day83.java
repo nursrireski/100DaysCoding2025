db7cbb8811f0994c1135fdc368872b9676734057
nursrireski <kikipolman2324@gmail.com>
2025-11-28 19:03:23 +0800

    Create day83.java

---
+++ day83.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class day83 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Tanya jumlah tamu
+        System.out.print("Berapa tamu yang ingin dicatat? ");
+        int jumlah = input.nextInt();
+        input.nextLine(); // membersihkan buffer
+
+        // Membuat array sesuai jumlah tamu
+        String[] tamu = new String[jumlah];
+
+        // Input nama tamu
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukkan nama tamu ke-" + (i+1) + ": ");
+            tamu[i] = input.nextLine();
+        }
+
+        // Output daftar tamu
+        System.out.println("\n--- Daftar Tamu Hadir ---");
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println((i+1) + ". " + tamu[i]);
+        }
+
+        
+    }
+}
