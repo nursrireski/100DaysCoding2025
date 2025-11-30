adf4dfad52b3d93e9a8d5b19beb1d6ec089d2537
nursrireski <kikipolman2324@gmail.com>
2025-11-30 21:37:29 +0800

    Create day84.java

---
+++ day84.java
@@ -0,0 +1,36 @@
+//soal1 
+import java.util.Scanner;
+public class day84 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <=10; i++) {
+            System.out.println(i + "x" + n + "=" + (i * n));
+
+        }
+
+    }
+}
+//soal2
+import java.util.Scanner;
+public class day84soal2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 1; i <= a; i++) {
+            System.out.println("Iccang");
+        }
+    }
+}
+//soal3
+import java.util.Scanner;
+public class day84soal3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String b = sc.nextLine();
+        int c = sc.nextInt();
+        for (int i = 1; i <= c; i++) {
+            System.out.println(b);
+        }
+    }
+}
