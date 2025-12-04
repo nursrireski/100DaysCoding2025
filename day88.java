b98d16cc0c0a0a599c52d81d3b4e195ea4a3554c
nursrireski <kikipolman2324@gmail.com>
2025-12-04 20:21:31 +0800

    Create day88.java

---
+++ day88.java
@@ -0,0 +1,10 @@
+public class day88 {
+    public static void main(String[] args) {
+        String[] buah = {"Apel", "Jeruk", "Mangga", "Pisang"};
+
+        // for-each
+        for (String b : buah) {
+            System.out.println(b);
+        }
+    }
+}
