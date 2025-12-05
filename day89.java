2b19cc4671dd62c58c5077a5eddfebdf9029b7f1
nursrireski <kikipolman2324@gmail.com>
2025-12-05 20:31:17 +0800

    Create day89.java

---
+++ day89.java
@@ -0,0 +1,12 @@
+public class day89 {
+    
+    // Method sapaDefault
+    public static void sapaDefault() {
+        System.out.println("Halo, selamat belajar Java!");
+    }
+
+    public static void main(String[] args) {
+        // Memanggil method sapaDefault
+        sapaDefault();
+    }
+}
