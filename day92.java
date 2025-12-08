29933b7de2d4c709586ec5cd6fd6123719426243
nursrireski <kikipolman2324@gmail.com>
2025-12-08 20:27:10 +0800

    Create day92.java

---
+++ day92.java
@@ -0,0 +1,9 @@
+public class day92 {
+    static int kurang(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kurang(10, 4)); 
+    }
+}
